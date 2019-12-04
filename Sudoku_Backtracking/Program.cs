9dd032f8d4cd875df98a0f73892528a99cfe47f7
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2019-12-04 10:28:13 +0200

    First commit

---
+++ Sudoku_Backtracking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sudoku_Backtracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
